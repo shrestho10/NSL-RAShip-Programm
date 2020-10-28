0b629edc88a9ee96b979cc8737e92af52b93544b
Shagoto Rahman <43316759+shrestho10@users.noreply.github.com>
2020-10-28 16:36:03 +0600

    Update Re.findall() & Re.finditer().py

--- python-basic/problem-Solving/Regex/Re.findall() & Re.finditer().py
+++ python-basic/problem-Solving/Regex/Re.findall() & Re.finditer().py
@@ -1,6 +1,10 @@
 import re
 s=input()
-a=re.findall('\w[a,e,i,o,u,A,E,I,O,U][a,e,i,o,u,A,E,I,O,U]+\w',s)
-for i in a:
-    p=re.findall('[a,e,i,o,u,A,E,I,O,U][a,e,i,o,u,A,E,I,O,U]+',i)
-    print(p[0])
+a=re.findall('[a,e,i,o,u,A,E,I,O,U][a,e,i,o,u,A,E,I,O,U]+[qwrtypsdfghjklzxcvbnm]',s)
+
+if len(a)==0:
+    print('-1')
+else:
+    for i in a:
+        p=re.findall('[a,e,i,o,u,A,E,I,O,U][a,e,i,o,u,A,E,I,O,U]+',i)
+        print(p[0])
